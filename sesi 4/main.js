b26e3f8ad034ed3be9211b8aef8cf9c83a5ce158
Onesinus SPT <onesinus@delman.io>
2022-08-28 22:15:18 +0700

    Sesi 4: javascript basic

---
+++ sesi 4/main.js
@@ -0,0 +1,15 @@
+// const => constant => nilainya tetap / tidak untuk diubah
+// let => yang nilainya bisa diubah2
+let name = 'Bambang'
+const usia = 17
+
+// Mengubah nilai variable yang dideklarasikan dengan let
+name = "Udin"
+
+// document.write(name + " -> Usia: " + usia)
+// alert(name + ", Usia: " + usia)
+// console.log(name + ", Usia: " + usia)
+// confirm("Apakah data sudah benar?")
+// prompt("Siapa namamu?")
+div_name = document.getElementById("div_name")
+div_name.innerHTML = "<i>Nama saya Ones</i>";
