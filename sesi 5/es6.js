fedbc2c93a6eb671dd769ee2c1aee4b6f3052a4b
Onesinus SPT <onesinus@delman.io>
2022-09-04 21:45:38 +0700

    ES5 VS ES6

---
+++ sesi 5/es6.js
@@ -0,0 +1,18 @@
+// ES 5
+var abc = "abc"
+console.log(abc)
+
+function helloWorld() {
+    console.log("hello world")
+}
+
+// ES 6
+let abc2 = "abc 2"
+console.log(abc2)
+
+const phi = 3.14
+console.log(phi)
+
+const helloWorldArrow = () => {
+    console.log("hello world")
+}
