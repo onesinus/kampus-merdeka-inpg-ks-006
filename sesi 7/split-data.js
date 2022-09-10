966f8fc458f288e90325aa0986b00443b1cc53e7
Onesinus SPT <onesinus@delman.io>
2022-09-10 21:34:44 +0700

    Split example

---
+++ sesi 7/split-data.js
@@ -0,0 +1,13 @@
+const nama = "Onesinus Saut Parulian Tamba"
+console.log(nama)
+console.log(typeof nama)
+
+const arr_nama = nama.split(" ")
+console.log(arr_nama)
+console.log(typeof arr_nama)
+
+const usia = "17,12,13,15".split(",")
+console.log(usia)
+
+const mobil = "BMW, Avanza, Calya".split(", ")
+console.log(mobil)
