1f9d03b9c66a87609280298252281ff486d6f663
Onesinus SPT <onesinus@delman.io>
2022-09-11 19:20:40 +0700

    object javascript

---
+++ sesi 8/latihan-object.js
@@ -0,0 +1,5 @@
+/*
+    buatlah sebuah objek untuk menampung data siswa, dimana didalamnya terdapat beberapa property seperti nama, program studi, smt, mata_kuliah (beberapa mata kuliah yang sedang ambil)
+    Tampilkan nama mahasiswa, program studi dan mata kuliah yang diambil
+    kemudian salah satu mata kuliah, dan tambahkan 1 mata kuliah
+*/
